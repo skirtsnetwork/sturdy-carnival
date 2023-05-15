02d04d7b735d306fb6a0acf720ee98cf92bbebe7
Skirt <126694961+skirtsnetwork@users.noreply.github.com>
2023-05-15 15:48:15 -0400

    Add files via upload

--- Ultraviolet-Static/public/uv/uv.config.js
+++ Ultraviolet-Static/public/uv/uv.config.js
@@ -1,12 +1,10 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-  prefix: "/service/",
-  bare: '/https://incog.dev/bare/',
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+    prefix: '/school/',
+    bare: 'https://incog.dev/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
 };
